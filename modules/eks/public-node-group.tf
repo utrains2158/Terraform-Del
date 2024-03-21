9accdbf71d051fb5666f7d77cb6257c19a4d15e8
samka6494 <utrains@outlook.com>
2024-03-21 15:59:30 -0500

    add eks module..

--- modules/eks/public-node-group.tf
+++ modules/eks/public-node-group.tf
@@ -30,7 +30,7 @@ resource "aws_eks_node_group" "eks_ng_public" {
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces...
   depends_on = [
     aws_iam_role_policy_attachment.eks-AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.eks-AmazonEKS_CNI_Policy,
